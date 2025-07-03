35a3a6c5642ccaddcc9756b672dd45e6d3a9c6e1
jesusflorestemahuay <jflorest@gmail.com>
2025-07-03 14:43:32 -0600

    Agregando mi info

--- Aboutus/Aboutus.js
+++ Aboutus/Aboutus.js
@@ -1,1 +1,16 @@
-const integrantes = [{foto:"/fotosEquipo/foto.jpg",NombreCompleto:"Jesus Cruz",información:"Egresado de escuela",biografia:"Pequeña descripción de nostros",rol:"Desarrollador FullStack"},{foto:"/fotosEquipo/foto.jpg",NombreCompleto:"Jesus Cruz",información:"Egresado de escuela",biografia:"Pequeña descripción de nostros",rol:"Desarrollador FullStack"}];
+const integrantes = [
+    {
+        foto:"/fotosEquipo/foto.jpg",
+        NombreCompleto:"Jesus Cruz",
+        información:"Egresado de escuela",
+        biografia:"Pequeña descripción de nostros",
+        rol:"Desarrollador FullStack"
+    },
+    {
+        foto:"https://media.licdn.com/dms/image/v2/D5603AQFOwM5sn_1wxw/profile-displayphoto-scale_200_200/B56Zeoj8f7HUAc-/0/1750879685295?e=1756944000&v=beta&t=aZviqYodpUsnSxjXxhjTl4U4X0oTppt51cn0bs2QqUw",
+        NombreCompleto:"Jesus Flores Temahuay",
+        información:"Desarrollador de Software Jr.",
+        biografia:"Ingeniero en Computación, apasionado por la tecnología y el aprendizaje continuo, con experiencia en desarrollo web, gestión de bases de datos y despliegue de sistemas en servidores.",
+        rol:"Desarrollador FullStack"
+    }
+        ];
