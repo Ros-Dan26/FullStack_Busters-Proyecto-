945fb14c098a5df7b87a764ff6739ada31d70a1c
Daniel_Arias <a.daniel_201.ar@outlook.com>
2025-07-23 23:58:31 -0600

    Cambios_Pag_Registro

--- Pagina_registro/registro2.js
+++ Pagina_registro/registro2.js
@@ -1,9 +1,10 @@
-/*
-document.addEventListener("DOMContentLoaded",() =>{
-    const btn = document.getElementById("enviar-registro");
 
-    btn.addEventListener("click", () => 
-    {
+document.getElementById('enviar-registro').addEventListener('submit', function (event) {
+    // Verificamos si el formulario es válido
+        event.preventDefault(); // Evita que se envíe el formulario
+   
+        // Aquí puedes ejecutar tu código solo si todo es válido
+
         const nombre = document.getElementById("nombre-registro").value;
         const apellidos = document.getElementById("apellidos-registro").value;
         const correo = document.getElementById("correo-registro").value;
@@ -11,20 +12,36 @@ document.addEventListener("DOMContentLoaded",() =>{
         const contrasena = document.getElementById("contrasena-registro").value;
         const contrasenaConf = document.getElementById("contrasenaConf-registro").value;
 
-        alert(`Nombre: ${nombre} Apellidos: ${apellidos}`);
-        alert(`Correo: ${correo} estado: ${estado}`);
-        alert(`contraseña: ${contrasena} confirma: ${contrasenaConf}`);
-        
-    })
-})*/
+        let resultadoIgualdad;
+
+        if (contrasena === contrasenaConf) {
+            resultadoIgualdad = "iguales"
+
+            //alert(`Nombre: ${nombre} Apellidos: ${apellidos}`);
+            //alert(`Correo: ${correo} estado: ${estado}`);
+            //alert(`contraseña: ${contrasena} confirma: ${contrasenaConf} comp: ${resultadoIgualdad}`);
+
+            mostrarPopupRegistro(event);
+        }
+        else {
+            resultadoIgualdad = "Diferentes"
+            alert("Contraseñas no coinciden.")
+        }
+
+        console.log("Formulario válido, ejecutando acción...");
+    
+});
+
+
 
 function mostrarPopupRegistro(event) {
     event.preventDefault();
-    document.getElementById("popup-registro").style.display="flex";
+    document.getElementById("popup-registro").style.display = "flex";
 }
 
 function cerrarPopupRegistro() {
-    document.getElementById("popup-registro").style.display="none";
+    document.getElementById("popup-registro").style.display = "none";
+    window.location.href = "login.html";
 }
 
 
