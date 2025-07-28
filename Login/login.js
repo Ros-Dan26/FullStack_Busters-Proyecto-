4c4d01353d5fc2c47fcff9903411566d0fa16694
brandon Hernandez <brandonpais14@gmail.com>
2025-07-28 13:14:16 -0600

    Redireccdion a pagina principal despues de un logiun exitoso

--- Login/login.js
+++ Login/login.js
@@ -3,11 +3,13 @@ document.getElementById('loginForm').addEventListener('submit', function (e) {
     const email = document.getElementById('email').value.trim();
     const password = document.getElementById('password').value.trim();
 
+    // Validacion de Inicio de Sesion
     if (email === "" || password === "") {
         alert("Por favor, completa todos los campos.");
         return;
     }
 
     // Aquí puedes agregar la lógica de autenticación
-    alert("¡Inicio de sesión exitoso!");
+    alert("¡Inicio de sesión exitoso!, Redirigiendo a la pagina principal");
+    window.location.href = 'index.html';
 });
