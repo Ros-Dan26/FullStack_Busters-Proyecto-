435df24f1bb3292bca01d14ce3dc0e269c2e7675
JesusFranciscoCruz <cruz.ramirez.jesus.francisco@gmail.com>
2025-08-05 10:12:56 -0600

    insert en base de datos (productos)

--- DB/netshopingonlinedb-insert.sql
+++ DB/netshopingonlinedb-insert.sql
@@ -3180,14 +3180,22 @@ INSERT INTO paypals(id_user, email) VALUES
 ('100', (select email from users where id=16));
 
 
+
 -- insert de productos
 insert into products (id_user,id_subcategory,id_status,id_size,id_gender_product,id_color,name,model,description,details,price,id_brand) 
 values(10,5,1,1,1,46,"Yeezy","Boost","tenis casual comodos","parte superior YEEZY Primekni",3500,2);
 
+insert into products (id_user,id_subcategory,id_status,id_size,id_gender_product,id_color,name,model,description,details,price,id_brand) 
+values(23,5,1,1,1,52," Kyrie 5","Nike SB Dunk Low Jarritos","Tenis de colección en colaboración con jarritos","Color amarillo con café",5510,1);
+
+insert into products (id_user,id_subcategory,id_status,id_size,id_gender_product,id_color,name,model,description,details,price,id_brand) 
+values(16,5,1,1,2,21," Kyrie 5","Tenis ZX 500 RM Goku de adidas x Dragon Ball","Tenis de colección en colaboración con Dragon Ball","Color Naranja",9577,2);
+
 -- insert de imagenes
-values (1,"https://cdn-images.farfetch-contents.com/17/03/72/28/17037228_35781287_600.jpg");
+insert into images_products(id_product,url) values (1,"https://cdn-images.farfetch-contents.com/17/03/72/28/17037228_35781287_600.jpg");
 
+insert into images_products(id_product,url) values (4,"https://acdn-us.mitiendanube.com/stores/003/194/492/products/16-10b0c4deb57f4e33b717304358564763-1024-1024.png");
 
-
+insert into images_products(id_product,url) values (5,"https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTjFuYc-Nn705WfWU_IDpgtrtc8T-8XNT-aObYnl5ZsRsfGZrFAsuTchuqWxR1sQ_8HIFrtjYkSB6xfL0c8Nb8NgY-PKfoYdrxtmu9sEs5Ago-kvoag0ej0i2E");
 
 
